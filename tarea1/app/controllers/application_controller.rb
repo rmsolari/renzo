7de894043efcfa6e29d75951114046eded3dbca8
rmsolari <rmsolari@uc.cl>
2018-03-17 02:33:04 -0300

    sadasdsada

--- tarea1/app/controllers/application_controller.rb
+++ tarea1/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world! shao gente me voy"
+    render html: "hello, world! chao people"
   end
 
 end
